e1b48cb117f55797195831482b6eac006f035b7f
nadiakramer <36862660+nadiakramer@users.noreply.github.com>
2025-10-31 21:48:44 +0100

    create landing page

--- code/frontend/app/pages/index.vue
+++ code/frontend/app/pages/index.vue
@@ -1,1 +1,9 @@
-<template>Index</template>
+<script setup>
+
+</script>
+<template>
+  <h1>Catch me if you can</h1>
+  <NuxtLink to="/lobby/create"><button>Lobby erstellen</button></NuxtLink>
+  <NuxtLink to="/lobby/join"><button>Lobby beitreten</button></NuxtLink>
+</template>
+
