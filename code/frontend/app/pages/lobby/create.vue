7658527bfc69491b33abfc809f21167b15816546
nadiakramer <36862660+nadiakramer@users.noreply.github.com>
2025-10-31 22:25:06 +0100

    lobby overview and create page

--- code/frontend/app/pages/lobby/create.vue
+++ code/frontend/app/pages/lobby/create.vue
@@ -1,1 +1,7 @@
-<template>Create Lobby</template>
+<script setup>
+</script>
+<template>
+  <h1>Lobby erstellen</h1>
+  <input />
+  <NuxtLink to="/lobby/overview"><button>erstellen</button></NuxtLink>
+</template>
--- code/frontend/app/pages/lobby/overview.vue
+++ code/frontend/app/pages/lobby/overview.vue
@@ -1,1 +1,21 @@
-<template>Overview</template>
+<script setup>
+
+</script>
+<template>
+  <h1>Lobby</h1>
+  <table>
+    <tr>
+      <th>
+        Nr.
+      </th>
+      <th>Name</th>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td>Marco Fabian</td>
+    </tr>
+  </table>
+  <button>
+    Start
+  </button>
+</template>
