eea7e74b7c0c305878d27b69e3d9421c977399a6
megiSW <marco.inniger@sitewerk.ch>
2025-10-31 22:31:56 +0100

    feat: add tailwindcss and shadcn-vue

--- code/frontend/nuxt.config.ts
+++ code/frontend/nuxt.config.ts
@@ -1,10 +1,16 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   ssr: false,
-  server: {
-    port: 3000,
+  modules: ["@nuxt/eslint", "@nuxt/image", "@nuxt/ui", "shadcn-nuxt"],
+  vite: {
+    plugins: [tailwindcss()],
   },
-  modules: ["@nuxt/eslint", "@nuxt/image", "@nuxt/ui"],
+  shadcn: {
+    prefix: "",
+    componentDir: "./components/ui",
+  },
 });
