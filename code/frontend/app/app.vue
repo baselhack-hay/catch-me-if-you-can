7d04baf66c2e4d6d20473125056fa477624dfee0
nadiakramer <36862660+nadiakramer@users.noreply.github.com>
2025-11-01 13:11:07 +0100

    feat: grainy background

--- code/frontend/app/app.vue
+++ code/frontend/app/app.vue
@@ -3,8 +3,7 @@ import "./globals.css";
 </script>
 
 <template>
-  <div class="font-joti">
-    <h1 class="text-4xl">Hello Nuxt</h1>
+  <div class="font-joti relative noise bg-(--color-chinese-black)">
     <NuxtPage />
   </div>
 </template>
