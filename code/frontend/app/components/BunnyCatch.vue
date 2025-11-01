a7db8459be83d64c4c7d1d3759f86f5c60934146
Levin Schaller <levin.schaller@coop.ch>
2025-11-01 20:17:50 +0100

    feat(app:supercard): TECHIAPPSUPERCARD-6767: Add bunny catch view

---
+++ code/frontend/app/components/BunnyCatch.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="h-screen flex flex-col justify-center items-center">
+    <div class="flex flex-col items-center justify-center text-center">
+      <UiTitle class="uppercase">
+        {{ props.variant === "bunny" ? "You were caught" : "You caught" }}
+      </UiTitle>
+
+      <img src="/img/bunny_caught.png" alt="Bunny" class="w-60 py-10" />
+
+      <UiTitle v-if="props.variant === 'bunny' && props.hunterName">
+        {{ "by " + props.hunterName.toUpperCase() }}
+      </UiTitle>
+      <UiTitle v-else-if="props.variant === 'hunter' && props.bunnyName">
+        {{ props.bunnyName.toUpperCase() }}
+      </UiTitle>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  variant: "hunter" | "bunny";
+  hunterName?: string;
+  bunnyName?: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<style scoped>
+.gradient::before {
+  background: linear-gradient(transparent 25%, var(--color-orange) 100%);
+}
+</style>
---
+++ code/frontend/app/pages/bunny-catch.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="h-screen relative gradient-base gradient"
+    :style="{
+      '--color-accent': role === 'hunter' ? '#970c02' : '#5e7601',
+    }"
+  >
+    <BunnyCatch
+      :variant="role"
+      :hunter-name="'big hunter'"
+      :bunny-name="'lil bunny'"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+const role = "hunter";
+</script>
+
+<style scoped>
+.gradient::before {
+  background: linear-gradient(transparent 25%, var(--color-accent) 100%);
+  pointer-events: none;
+}
+</style>
