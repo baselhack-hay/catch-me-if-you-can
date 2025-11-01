9f515edb4c466cd75c08b87ae9b243733c7a6090
Levin Schaller <levin.schaller@coop.ch>
2025-11-01 15:05:07 +0100

    fix: Fix button warning

--- code/frontend/app/components/ui/button/Button.vue
+++ code/frontend/app/components/ui/button/Button.vue
@@ -4,7 +4,7 @@
     <div
       class="absolute top-0.5 left-0 w-full h-full rounded-md z-0"
       :class="getUnderLayerClass(variant)"
-    ></div>
+    />
 
     <!-- button itself -->
     <Primitive
@@ -40,7 +40,7 @@ const props = withDefaults(defineProps<Props>(), {
 const { variant, size } = props;
 
 // function to get the class for the shadow effect
-const getUnderLayerClass = (variant?: string) => {
+const getUnderLayerClass = (variant?: string | null) => {
   switch (variant) {
     case "default":
       return "bg-gradient-to-r from-(--color-selective-yellow) to-(--color-orange)";
